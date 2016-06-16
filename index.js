174038cf56ecd2697347ffa8875d84b6bd1c3cb2
Anubhav Agarwal <Viper@Anubhavs-Air.Home>
2016-06-17 01:11:45 +0200

    Changed start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var oauth ={ client_id: 'f5fab340aebab3b77bcd', redirect_uri: "https//:example.com"}
+var oauth ={ client_id: 'f5fab340aebab3b77bcd', redirect_uri: "https://git-oauth.herokuapp.com/repos"}
 var url = 'https://github.com/login/oauth/authorize?client_id='+oauth.client_id+'&redirect_uri='+oauth.redirect_uri
 var port = process.env.PORT || 80;
 
