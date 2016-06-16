3fc9f28eadf305177298b08020ab2f5497e0d5d5
Anubhav Agarwal <Viper@Anubhavs-Air.Home>
2016-06-17 00:57:20 +0200

    Created basic setup

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var oauth ={ client_id: 'f5fab340aebab3b77bcd', redirect_uri: "https//:example.com"}
+var url = 'https://github.com/login/oauth/authorize?client_id='+oauth.client_id+'&redirect_uri='+oauth.redirect_uri
+var port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+    res.render('index',{url:url});
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
